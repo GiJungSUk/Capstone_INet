26ce81324f7dc681627cc1a726d692a0c6300e82
GiJungSUk <kim05220808@gmail.com>
2023-05-24 18:29:42 +0900

    첫 프로젝트 업로드입니다

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public string sceneName;
+
+    public void SceneChanger()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+
+}
